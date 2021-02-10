47600269927819a43b9f9a483639263018001793
Jblegoff <47114588+Jblegoff@users.noreply.github.com>
2021-02-10 22:51:45 +0100

    deployement commit

---
+++ Assets/Script/CameraMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    [SerializeField] private Camera m_camera;
+    [SerializeField] private Transform target;
+
+    private Vector3 previousPosition;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            previousPosition = m_camera.ScreenToViewportPoint(Input.mousePosition);
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            Vector3 direction = previousPosition - m_camera.ScreenToViewportPoint(Input.mousePosition);
+            m_camera.transform.position = target.position;
+            m_camera.transform.Rotate(new Vector3(1, 0, 0), direction.y* 180);
+            m_camera.transform.Rotate(new Vector3(0, 1, 0), -direction.x * 180,Space.World);
+            m_camera.transform.Translate(new Vector3(0, 0, -2));
+
+            previousPosition = m_camera.ScreenToViewportPoint(Input.mousePosition);
+        }
+    }
+}
---
+++ Assets/Script/LoadAssetBundles.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.Networking;
+
+public class LoadAssetBundles : MonoBehaviour
+{
+    AssetBundle m_loadedAssetBundle;
+    string url = "https://storage.googleapis.com/dviewer-59182.appspot.com/food"; 
+    public string[] assetsName;
+    int index = 0;
+    GameObject instancedPrefab;
+    
+
+    void Start()
+    {
+        StartCoroutine(LoadAssetBundle());
+       
+    }
+
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+
+            DestroyObject();
+            if (index == assetsName.Length - 1) index = 0;
+            else index++;
+            Debug.Log("index= " + index);
+            instancedPrefab = InstantialteObjectFromBundle(assetsName[index]);
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+
+            DestroyObject();
+            if (index == 0) index = assetsName.Length - 1;
+            else index--;
+            Debug.Log("index= " + index);
+            instancedPrefab = InstantialteObjectFromBundle(assetsName[index]);
+        }
+       
+
+    }
+
+    GameObject InstantialteObjectFromBundle(string assetName)
+    {
+        var prefab = m_loadedAssetBundle.LoadAsset(assetName);
+        GameObject instancedPrefab = (GameObject)Instantiate(prefab);
+        return instancedPrefab;
+    }
+    void DestroyObject()
+    {
+        Destroy(instancedPrefab);
+    }
+    IEnumerator LoadAssetBundle()
+    {
+        UnityWebRequest uwr = UnityWebRequestAssetBundle.GetAssetBundle(url);
+
+        yield return uwr.SendWebRequest();
+
+            if (uwr.isNetworkError || uwr.isHttpError)
+            {
+                Debug.Log(uwr.error);
+            }
+            else
+            {
+                // Get downloaded asset bundle
+              
+                m_loadedAssetBundle = DownloadHandlerAssetBundle.GetContent(uwr);
+
+                assetsName = new string[m_loadedAssetBundle.GetAllAssetNames().Length];
+                assetsName = m_loadedAssetBundle.GetAllAssetNames();
+
+                instancedPrefab = InstantialteObjectFromBundle(assetsName[index]);
+            }
+        
+    }
+   
+}
+
+
